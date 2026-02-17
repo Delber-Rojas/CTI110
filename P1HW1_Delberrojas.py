9bb2fdf1ee58d74a1352fd9c9edb070a850e0fae
Delber-Rojas <rojasd6649@student.faytechcc.edu>
2026-02-17 14:38:08 -0500

    P1HW1

---
+++ P1HW1_Delberrojas.py
@@ -0,0 +1,30 @@
+#Delber
+#2/17/2026
+#P1HW1 LAb
+# for this assignment you will write a python program that uses mathematical experessions 
+
+
+print("----- Calculating Exponents----")
+print("_" * 5 + "Calculating Exponents" + "_" * 4)
+print()
+print()
+base = int(input("Enter an integer the base value: "))
+exponent = int(input("Enter an interger as the base value: "))
+print()
+print()
+#number = base ** exponent
+#print(base, "raised to power of", exponent, "is",number)
+print(base, "raised to power of", exponent, "is",base ** exponent, "!!")
+print()
+print()
+print("-----Addition and Subtraction-----")
+print()
+print()
+start =int(input("Enter a starting interger: "))
+add =int(input("Enter an integer to add "))
+subtract =int(input("Enter an integer to Subtract "))
+print()
+print()
+print(start, "+" ,add, "_",subtract, "is equal to", start + add - subtract)
+
+
