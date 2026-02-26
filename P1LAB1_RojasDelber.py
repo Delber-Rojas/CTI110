4878788f29b525fca6047ab1f8fb71b1bd516344
Delber-Rojas <rojasd6649@student.faytechcc.edu>
2026-02-25 22:52:04 -0500

    Refactor main function and add header comments

--- P1LAB1_RojasDelber.py
+++ P1LAB1_RojasDelber.py
@@ -1,7 +1,13 @@
+#Delber Rojas 
+# 2/25/2026
+# The goal is to get input from the user and it displays output
+
+
+#input#first name#input#last
+
 def main():
 	first_name = input('Enter your first name: ')
 	last_name = input('Enter your last name: ')
-	# Normalize capitalization and remove surrounding whitespace
 	first_name = first_name.strip().title()
 	last_name = last_name.strip().title()
 	print(f'Hello, {first_name} {last_name}! Welcome to CTI-110')
@@ -9,4 +15,5 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
 
